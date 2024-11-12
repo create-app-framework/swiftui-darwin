ec41b5028f5ebfc1303bd2ba67b233d9917bddaf
yizhou <tongyz2922@mails.jlu.edu.cn>
2024-11-12 22:36:07 +0800

    Initial Commit

---
+++ MyApplicationTests/MyApplicationTests.swift
@@ -0,0 +1,16 @@
+//
+//  MyApplicationTests.swift
+//  MyApplicationTests
+//
+//  Created by Don Yihtseu on 12/11/2024.
+//
+
+import Testing
+
+struct MyApplicationTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
